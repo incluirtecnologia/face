68d18126204b39f348739640698eae6abab9e9d5
Fernando Freitas <fernandoafreitas87@gmail.com>
2021-07-05 15:51:37 -0300

    requeriments

---
+++ test-gpu.py
@@ -0,0 +1,8 @@
+# Definindo dispositivo de hardware
+if torch.cuda.is_available():
+    args['device'] = torch.device('cuda')
+else:
+    args['device'] = torch.device('cpu')
+
+print(args['device'])
+warnings.filterwarnings("ignore")
