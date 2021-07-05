c8b9306cdccbaf82a68973d08e3bbc414f0fd15e
Fernando Freitas <fernandoafreitas87@gmail.com>
2021-07-05 16:54:08 -0300

    update do test-gpu2

--- test-gpu.py
+++ test-gpu.py
@@ -1,5 +1,12 @@
-
 import torch
+
+# Configurando hiperparâmetros.
+args = {
+    'epoch_num': 150,     # Número de épocas.
+    'lr': 1e-3,           # Taxa de aprendizado.
+    'weight_decay': 1e-3, # Penalidade L2 (Regularização).
+    'batch_size': 100,     # Tamanho do batch.
+}
 # Definindo dispositivo de hardware
 if torch.cuda.is_available():
     args['device'] = torch.device('cuda')
